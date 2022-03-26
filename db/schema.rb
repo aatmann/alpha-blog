fdcb2ff50f4bfc087b37b7d39826ffa61cd200e8
i-Sapna <derinaig@gmail.com>
2022-03-26 21:03:15 +0530

    Add has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_26_141224) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_26_152513) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_26_141224) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
